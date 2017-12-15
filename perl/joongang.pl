13efa7709e3eb4bc167076a14bb5347bbef2ada7
eunmo <eunmo@cs.stanford.edu>
2017-12-16 02:23:49 +0900

    handle exception for articles with photo for joongang

--- perl/joongang.pl
+++ perl/joongang.pl
@@ -18,14 +18,21 @@ my $count = 0;
 my $ul = $dom->find('ul[class~="type13"]')->first;
 
 for my $li ($ul->find('li')->each) {
-	
-	my $a = $li->find('a')->first;
-	my $href = $a->attr('href');
-	my $title = $a->all_text;
-	$href =~ s/http:\/\/news.naver.com//;
 
-	$json .= "," if $count++;
-	$json .= "{\"href\": \"$href\", \"title\": \"$title\"}";
+	for my $dt ($li->find('dt')->each) {
+
+		next if $dt->attr('class') =~ "photo";
+	
+		my $a = $dt->find('a')->first;
+		my $href = $a->attr('href');
+		my $title = $a->all_text;
+		$href =~ s/http:\/\/news.naver.com//;
+
+		$json .= "," if $count++;
+		$json .= "{\"href\": \"$href\", \"title\": \"$title\"}";
+
+		last;
+	}
 }
 
 $json .= "]";
