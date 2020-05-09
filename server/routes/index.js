233c9209ca5a7b159bfa00e7a531268d20c9bcfe
eunmo <eunmo@cs.stanford.edu>
2020-05-09 22:44:00 +0900

    clean up server/routes/index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,15 +5,13 @@ const clear = require('./clear');
 const fetch = require('./fetch');
 const select = require('./select');
 
-module.exports = () => {
-  const router = express.Router();
-  clear(router);
-  fetch(router);
-  select(router);
+const router = express.Router();
+clear(router);
+fetch(router);
+select(router);
 
-  router.get('*', (req, res) => {
-    res.sendFile(path.resolve('build', 'index.html'));
-  });
+router.get('*', (req, res) => {
+  res.sendFile(path.resolve('build', 'index.html'));
+});
 
-  return router;
-};
+module.exports = router;
