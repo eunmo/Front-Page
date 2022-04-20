168fd81af1887116459997ee8172d4392a77abf7
eunmo <eunmo@cs.stanford.edu>
2022-04-20 16:16:54 +0000

    react 18

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
-import { render } from 'react-dom';
-import App from './App';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom';
 import './index.css';
+import App from './App';
 
-render(<App />, document.getElementById('root'));
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
